c6a0987ffaacc2c57546df71b2908faebd1d9502
Donnie Mattingly <donniewmattingly@gmail.com>
2021-12-15 15:57:17 -0500

    day 15 partial

--- lib/day15.ex
+++ lib/day15.ex
@@ -28,20 +28,54 @@ defmodule Day15 do
     |> Utils.list_of_lists_to_map_by_point()
   end
 
-  def solve(input) do
-    goal = input |> Map.to_list |> Enum.map(&elem(&1, 0)) |> Enum.max
-    paths = Utils.Graph.dijkstra({0,0}, &neighbor_for_point(input, &1), fn _, x -> Map.get(input, x) end)
+  def solve4(input) do
+    goal = input
+           |> Map.to_list
+           |> Enum.map(&elem(&1, 0))
+           |> Enum.max
+    paths = Utils.Graph.dijkstra({0, 0}, &neighbor_for_point(input, &1), &weight_for_point(input, &1, &2))
 
     [start | rest] = Utils.Graph.get_path(paths, goal)
 
-    paths |> IO.inspect
+    paths
+    |> IO.inspect
     rest
     |> Enum.map(&Map.get(input, &1))
     |> IO.inspect
     |> Enum.sum()
   end
 
+  def solve(input) do
+    g = make_graph(input)
+    goal = Map.keys(input)
+           |> Enum.max
+    path = Graph.dijkstra(g, {0, 0}, goal)
+
+    cost = path
+           |> Enum.map(&Map.get(input, &1))
+           |> Enum.sum
+
+    cost - Map.get(input, {0, 0})
+  end
+
+  def make_graph(input) do
+    edges = input
+            |> Map.keys()
+            |> Enum.flat_map(
+                 fn p ->
+                   Day15.neighbor_for_point(input, p)
+                   |> Enum.map(&{p, &1})
+                 end
+               )
+            |> Enum.map(fn {v, w} -> {v, w, Map.get(input, w)} end)
+            |> Enum.map(fn {a, b, w} -> Graph.Edge.new(a, b, weight: w) end)
+
+    Graph.new()
+    |> Graph.add_edges(edges)
+  end
+
   def neighbor_for_point(map, {x, y}) do
+
     [{0, 1}, {0, -1}, {1, 0}, {-1, 0}]
     |> Enum.map(
          fn
@@ -51,5 +85,21 @@ defmodule Day15 do
     |> Enum.filter(fn p -> Map.has_key?(map, p) end)
   end
 
-  def weight_for_point(map, point), do: Map.get(map, point)
+  def get_tiled_pos(tile_size, pos) do
+    {div(pos, tile_size + 1), rem(pos, tile_size + 1)}
+  end
+
+  def get_tiled_point(map, {x, y}) do
+    {chunk_size, _} = Map.keys(map)
+                      |> Enum.max()
+
+    {get_tiled_pos(chunk_size, x), get_tiled_pos(chunk_size, y)}
+  end
+
+  def in_expanded_map?(map, point) do
+    {{tx, ix}, {ty, iy}} = get_tiled_point(map, point)
+    Map.has_key?(map, {ix, iy}) and (tx < 5 and ty < 5)
+  end
+
+  def weight_for_point(map, from, point), do: Map.get(map, point)
 end
